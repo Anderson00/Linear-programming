bf075616b09e4ef25f351e19865218ba8659d094
Anderson <anderson.hld@outlook.com>
2019-06-22 14:26:41 -0300

    Initial Commit
    Arquivos iniciais

---
+++ Makefile
@@ -0,0 +1,18 @@
+CC = g++
+CFLAGS1 = -c -m64 -O -fPIC -fexceptions -DNDEBUG -DIL_STD -DILOSTRICTPOD
+CFLAGS2 =    -m64 -O -O0 -fPIC -fexceptions -DNDEBUG -DIL_STD -DILOSTRICTPOD -Wall
+LIBS =  -lm 
+INCLUDES = -I/opt/ibm/ILOG/CPLEX_Studio1261/cplex/include/ -I/opt/ibm/ILOG/CPLEX_Studio1261/concert/include/
+OBJS = readFile.o bitMapGraph.o PL.o
+SRCS = readFile.c bitMapGraph.c PL.cpp
+LIBS = -L/opt/ibm/ILOG/CPLEX_Studio1261/cplex/lib/x86-64_linux/static_pic -lilocplex -lcplex -L/opt/ibm/ILOG/CPLEX_Studio1261/concert/lib/x86-64_linux/static_pic -lconcert -lm -lpthread
+HDRS =  
+EXEC = PL
+
+compila: PL.cpp
+#para compilar no lia no cplex versão 11
+	${CC} ${CFLAGS1} ${INCLUDES} ${SRCS}
+	${CC} ${CFLAGS2} ${INCLUDES} ${OBJS} -o ${EXEC} ${LIBS}
+
+executa: compila
+	./PL ./instances/1-FullIns_3.col
---
+++ PL.cpp
@@ -0,0 +1,190 @@
+#include <stdio.h>
+#include <time.h>
+#include <iostream>
+#include <limits.h>
+#include <math.h>
+#include <ilcplex/ilocplex.h>
+#include <vector>
+#include <list>
+#include"readFile.h"
+
+#define eps 0.000001
+
+ILOSTLBEGIN
+using namespace std;
+
+int n,m;
+IloEnv   env;
+IloModel model(env);
+IloNumVarArray var_x(env);
+IloObjective obj = IloMaximize(env);
+IloCplex cplex(env);
+
+void createModel() {
+    IloExpr exp(env);
+    IloRangeArray con(env);
+
+    try {
+	double coef[n];
+
+        //printf("Creating %d X variables...\n");
+        for(int i=0;i<n;i++) {
+                char nome[20];
+                sprintf(nome,"x%d",i+1);
+                var_x.add(IloNumVar(env,0,IloInfinity));
+                var_x[i].setName(nome);
+        }
+
+        //printf("Creating Objective Function...\n\n");
+        coef[0]=3;
+        coef[1]=5;
+        for(int i=0;i<n;i++) obj.setLinearCoef(var_x[i], coef[i]);
+
+        //printf("Adding NO two classes constraints...\n\n");
+	exp += var_x[0];
+        con.add(IloRange(env, exp, 4));
+        exp.clear();
+
+        exp += 2*var_x[1];
+        con.add(IloRange(env, exp, 12));
+        exp.clear();
+
+	exp += 3*var_x[0] + 2*var_x[1];
+        con.add(IloRange(env, exp, 18));
+        exp.clear();
+
+        model.add(con);
+        con.clear();
+        model.add(obj);
+        cplex.extract(model);
+        cplex.exportModel("PL.lp");
+    }
+    catch (IloException& e) {
+        cerr << "Concert exception caught: " << e << endl;
+    }
+    catch (...) {
+        cerr << "Unknown exception caught" << endl;
+    }
+}
+
+void createModel2(){
+    IloExpr exp(env);
+    IloRangeArray con(env);
+
+
+    try{
+        double coef[2];
+        for(int i = 0; i < 2; i++){
+            char name[20];
+            sprintf(name, "x%d", i + 1);
+            var_x.add(IloNumVar(env,0,IloInfinity));
+            var_x[i].setName(name);
+        }
+        coef[0] = 2;
+        coef[1] = 3;
+
+        for(int i = 0; i < 2; i++) obj.setLinearCoef(var_x[i], coef[i]);
+
+        
+        exp += -1*var_x[0] + 2*var_x[1];
+        con.add(IloRange(env,exp, 4));
+        exp.clear();
+
+        exp += var_x[0] + 2*var_x[1];
+        con.add(IloRange(env,exp, 6));
+        exp.clear();
+
+        exp += 1*var_x[0] + 3*var_x[1];
+        con.add(IloRange(env,exp, 9));
+        exp.clear();
+
+        model.add(con);
+        con.clear();
+        model.add(obj);
+        cplex.extract(model);
+        cplex.exportModel("PL2.lp");
+
+    }catch (IloException& e) {
+        cerr << "Concert exception caught: " << e << endl;
+    }
+    catch (...) {
+        cerr << "Unknown exception caught" << endl;
+    }
+}
+
+bool solveMaster(double *x, double *sol_val) {    // Return true if the master problem is feasible
+    // Optimize the problem
+    if ( !cplex.solve() && cplex.getStatus() != IloAlgorithm::Infeasible) {
+        env.error() << "Failed to optimize LP" << endl;
+        cout << cplex.getStatus() << endl;
+        throw(-1);
+    }
+
+    // Get solution
+    if(cplex.getStatus() != IloAlgorithm::Infeasible) {
+        *sol_val = cplex.getObjValue();
+        //printf("\nSolution value = %.2lf ",*sol_val);
+        for(int i=0;i<n;i++)
+            x[i] = cplex.getValue(var_x[i]);
+        return true;
+    }
+    return false;
+}
+
+void Procedure() {
+    double sol_val=-1;
+    double *x=NULL;
+
+    x=(double*) malloc((n)*sizeof(double));    
+    for(int i=0;i<n;i++) memset(x,0.0,n*sizeof(double));
+
+    if(!solveMaster(x,&sol_val)) printf("INFEASIBLE!\n");   // Return false if the master problem is infeasible
+
+    printf("Objective Function Value = %.2lf\n",sol_val);
+    for(int i=0;i<n;i++)
+	printf("x[%d] = %2.lf\n", i+1, x[i]);
+
+    free(x);
+}
+
+
+int main (int argc, char *argv[]) {
+    /*
+    if (argc < 2) {
+        printf("Please, specify the file.\n");
+        return 0;
+    }
+    FILE * pFile = fopen(argv[1], "r");
+    if (pFile == NULL) {
+        printf("Could not open %s.\n", argv[1]);
+        return 0;
+    }
+
+    if (readFile(pFile, graph, &n, &m)) {
+        fclose(pFile);
+        printf("Could not read file %s.", argv[1]);
+        return 0;
+    }
+    fclose(pFile);*/
+
+    n=2;	// REMOVE THIS LINE TO START YOUR WORK!
+
+    clock_t start, end;
+    double elapsed;
+    start = clock();
+    // Solving the problem
+    cplex.setOut(env.getNullStream());      // Do not print cplex informations
+    cplex.setWarning(env.getNullStream());  // Do not print cplex warnings
+    createModel2();
+    Procedure();
+
+    // Free memory
+
+    // Getting run time
+    env.end();
+    end = clock();
+    elapsed = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("Time: %.5g second(s).\n", elapsed);
+
+    return 0;
+}
